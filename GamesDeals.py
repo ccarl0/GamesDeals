efabdfda7764e9524a4695edcd1040d1a0b43af9
Carlo <55291106+carloCecchetti@users.noreply.github.com>
2021-01-04 21:48:34 +0100

    Add files via upload

--- GamesDeals.py
+++ GamesDeals.py
@@ -18,56 +18,63 @@ for dict in json:
     titles_list.append(json[len(titles_list)].get('title'))
 
 i = 0
+
+
 def next_page():
     global i
     i += 1
     get_games_news(i)
+
 
 def previous_page():
     global i
     i -= 1
     get_games_news(i)
 
+
 def get_games_news(i):
     global game_link, json
 
-    #Game's title
-    games_title_label['text'] = f"{titles_list.index(json[i-1]['title'])+1}. {json[i-1]['title']} \n"
-    #Getting game link
-    game_link = json[i]['open_giveaway_url']
-    #Getting game description
-    game_desc_label['text'] = f"{json[i-1]['description']}\n"
-    #Thumbnail
-    urllib.request.urlretrieve(json[i-1]['thumbnail'],"thumbnail.png")
-    print(json[i-1]['thumbnail'])
+    # Game's title
+    games_title_label['text'] = f"{titles_list.index(json[i - 1]['title']) + 1}. {json[i - 1]['title']} \n"
+    # Getting game link
+    game_link = json[i - 1]['open_giveaway_url']
+    # Getting game description
+    game_desc_label['text'] = f"{json[i - 1]['description']}\n"
+    # Thumbnail
+    urllib.request.urlretrieve(json[i - 1]['thumbnail'], "thumbnail.png")
+    print(json[i - 1]['thumbnail'])
+
 
 def link(url):
     webbrowser.open_new(url)
 
-#-----------------------------------------------------------------------------------------------------------------------
+
+# -----------------------------------------------------------------------------------------------------------------------
 
 root = tk.Tk()
-root.geometry("1600x800") #tab x,y
+root.geometry("1600x800")  # tab x,y
 root.title("XMy Projects")
 root.iconphoto(False, tk.PhotoImage(file="icon.png"))
 
-frame = tk.Frame(root, bg = "#0d001a")
+frame = tk.Frame(root, bg="#0d001a")
 frame.place(relwidth=1, relheight=1)
-#Page Buttons
-previous_page_button_side = tk.Button(frame, text = "Previous Page", command = lambda : previous_page(), heigh = 12, width = 11, font=("Arial", 12), bg = "#ffb3ff", activebackground = "#660066")
-previous_page_button_side.pack(side = tk.LEFT)
+# Page Buttons
+previous_page_button_side = tk.Button(frame, text="Previous Page", command=lambda: previous_page(), heigh=12, width=11,
+                                      font=("Arial", 12), bg="#ffb3ff", activebackground="#660066")
+previous_page_button_side.pack(side=tk.LEFT)
 
-next_page_button_side = tk.Button(frame, text = "Next Page", command = lambda : next_page(), heigh = 12, width = 11, font=("Arial", 12), bg = "#ffb3ff", activebackground = "#4d004d")
-next_page_button_side.pack(side = tk.RIGHT)
-#Game's title
-games_title_label = tk.Label(frame, text = "", fg = "#d279a6", bg = "#0d001a", font=("Helvetica", 40), heigh = 4)
+next_page_button_side = tk.Button(frame, text="Next Page", command=lambda: next_page(), heigh=12, width=11,
+                                  font=("Arial", 12), bg="#ffb3ff", activebackground="#4d004d")
+next_page_button_side.pack(side=tk.RIGHT)
+# Game's title
+games_title_label = tk.Label(frame, text="", fg="#d279a6", bg="#0d001a", font=("Helvetica", 40), heigh=4)
 games_title_label.pack()
-#Game's description
-game_desc_label = tk.Label(frame, text = "", fg = "#6666ff", bg = "#0d001a", font=("Arial", 24), wraplength = 700)
+# Game's description
+game_desc_label = tk.Label(frame, text="", fg="#6666ff", bg="#0d001a", font=("Arial", 24), wraplength=700)
 game_desc_label.pack()
-#Link to deal webpage
+# Link to deal webpage
 game_link = ""
 games_title_label.bind("<Button-1>", lambda e: link(game_link))
-
 
 root.mainloop()
